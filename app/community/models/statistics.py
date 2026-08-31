4afbb99dd3d74c33ae47fe86625c0431618ba456
Akshaykumar Maldhure <akshaykumarmaldhure@MacBookPro.bwrouter>
2026-08-31 13:34:48 +0530

    Display app statistics

--- app/community/models/statistics.py
+++ app/community/models/statistics.py
@@ -4,12 +4,12 @@ class CommunityStatistics(BaseModel):
     """
     Model representing dynamic community statistics.
     """
-    members: str = Field(default="0", description="Total members count")
-    activeProfiles: str = Field(default="0", description="Total active profiles count")
-    doctors: str = Field(default="0", description="Total doctors count")
-    engineers: str = Field(default="0", description="Total engineers count")
-    new: str = Field(default="0", description="Total new profiles count")
-    verified: str = Field(default="0", description="Total verified profiles count")
+    members: int = Field(default=0, description="Total members count")
+    activeProfiles: int = Field(default=0, description="Total active profiles count")
+    doctors: int = Field(default=0, description="Total doctors count")
+    engineers: int = Field(default=0, description="Total engineers count")
+    new: int = Field(default=0, description="Total new profiles count")
+    verified: int = Field(default=0, description="Total verified profiles count")
 
 class CommunityStatisticsDB(CommunityStatistics):
     """
